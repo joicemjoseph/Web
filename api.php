3640f7bc0d3e48cc493c160f0216657df3d98651
Joice Joseph <joicefootball@gmail.com>
2009-05-10 16:14:21 +0100

    Images also updated to the repo

--- api.php
+++ api.php
@@ -9,9 +9,9 @@ correct values.
 
 require_once('lib/nusoap.php');
 
-$username1 = "janeesh";
-$password1 = "tasc787captain";
-$senderid1 = "QREAD";//this will be for gsm numbers, you can set the cdma sender id in the website. GSM sender id should be max 11 chars
+$username1 = "";
+$password1 = "";
+$senderid1 = "GHOSTBOYS";//this will be for gsm numbers, you can set the cdma sender id in the website. GSM sender id should be max 11 chars
 $api_path = "http://api.infinitesms.in/webservice/falertservicerevised.php?wsdl";
 $client = new soapclient1($api_path, true);
 $param = array('username'=>$username1, 'senderid'=>$senderid1, 'password'=>$password1, 'message'=>$message, 'number'=>$numbers);
